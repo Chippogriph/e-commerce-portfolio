8f4186c6f34d3396b6c1dedd57c8909dbebfa920
Christoffer Persson <christoffer-persson@hotmail.se>
2025-04-28 18:41:32 +0200

    Add product detail view

--- client/src/app/routes/product-details/product-details.component.ts
+++ client/src/app/routes/product-details/product-details.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Product, ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCarouselComponent } from '../../shared/components/product-carousel/product-carousel.component';
+import { CartItems, CartService } from '../../services/cart.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product | null = null;
+  cartItems: CartItems[] = [];
+  isLoading = true;
+  errorMessage: string | null = null;
 
+  private productService = inject(ProductService);
+  private route = inject(ActivatedRoute);
+  private cartService = inject(CartService);
+  private titleService = inject(Title)
+
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const slug = params.get('slug');
+      if (slug) {
+        this.productService.getProductBySlug(slug).subscribe({
+          next: (data) => {
+            this.product = data;
+            this.titleService.setTitle(this.product.name);
+            this.isLoading = false;
+          },
+          error: (error) => {
+            console.error('Fel vid hämtning av produkt:', error);
+            this.errorMessage = 'Produkten kunde inte hämtas.';
+            this.isLoading = false;
+          },
+        });
+      }
+    });
+  }
+
+  
+
+  addProductToCart(product: Product) {
+    this.cartService.addToCart({
+      id: 0,
+      slug: product.slug,
+      name: product.name,
+      url: product.url,
+      brand: product.brand,
+      price: product.price,
+      quantity: 1
+    }).subscribe({
+      next: (updatedCart) => {
+        console.log('Varukorgen uppdaterad:', updatedCart);
+      },
+      error: (err) => {
+        console.error('Fel vid tillägg till varukorg:', err);
+      }
+    });
+  }
 }
---
+++ client/src/app/shared/components/product-carousel/product-carousel.component.ts
@@ -0,0 +1,54 @@
+import { Component, inject } from '@angular/core';
+import { Product, ProductService } from '../../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-product-carousel',
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
+  templateUrl: './product-carousel.component.html',
+  styleUrl: './product-carousel.component.css',
+})
+export class ProductCarouselComponent {
+  products: Product[] = [];
+  isLoading = true;
+  currentIndex = 0;
+
+  private productService = inject(ProductService)
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Fel vid hämtning av produkter:', error);
+        this.isLoading = false;
+      },
+    });
+  }
+
+  get transform(): string {
+    return `-${this.currentIndex * 33.33}%`;
+  }
+
+  next() {
+    if (this.currentIndex < this.products.length / 3 - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = Math.floor(this.products.length / 3) - 1;
+    }
+  }
+}
