a5b67f557f8bb032c397bd3c766aec15a0f6490c
Christoffer Persson <christoffer-persson@hotmail.se>
2025-08-27 10:06:33 +0200

    Iplement feature/cart count icon

--- client/src/app/layout/public/site-header/site-header.component.ts
+++ client/src/app/layout/public/site-header/site-header.component.ts
@@ -2,26 +2,31 @@ import { Component, inject } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CartService } from '../../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-site-header',
-  imports: [
-    FormsModule,
-    NavbarComponent,
-    RouterLink],
+  imports: [CommonModule, FormsModule, NavbarComponent, RouterLink],
   templateUrl: './site-header.component.html',
-  styleUrl: './site-header.component.css'
+  styleUrl: './site-header.component.css',
 })
 export class SiteHeaderComponent {
   searchQuery = '';
+  cartCount = 0;
 
-  
-  private router = inject(Router)
-  
+  private cartService = inject(CartService);
+  private router = inject(Router);
+
+  ngOnInit() {
+    this.cartService.cartCount$.subscribe((count) => {
+      this.cartCount = count;
+    });
+  }
+
   onSearch(event: Event) {
     event.preventDefault();
     this.router.navigate(['/search'], { queryParams: { q: this.searchQuery } });
-    this.searchQuery = "";
+    this.searchQuery = '';
   }
-  
 }
