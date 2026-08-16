1cda252f57c4abc2e73ac6b786e244bf396dfa38
Christoffer Persson <christoffer-persson@hotmail.se>
2026-08-16 21:57:27 +0200

    Fix: forgot to add import

--- client/src/app/routes/home/spots/spots.component.ts
+++ client/src/app/routes/home/spots/spots.component.ts
@@ -2,15 +2,15 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CategoriesService } from '../../../services/categories/categories.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FullImageUrlPipe } from '../../../shared/pipes/full-image-url.pipe';
 
 @Component({
   selector: 'app-spots',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FullImageUrlPipe],
   templateUrl: './spots.component.html',
-  styleUrl: './spots.component.css'
+  styleUrl: './spots.component.css',
 })
 export class SpotsComponent implements OnInit {
-
   private categoriesService = inject(CategoriesService);
   categories$ = this.categoriesService.categories$;
 
